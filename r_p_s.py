7631f74a086c9e6e0731b131361b153072980968
Azlan <md.hossain-2@student.manchester.ac.uk>
2020-12-19 20:35:18 +0000

    Add quit functionality

--- r_p_s.py
+++ r_p_s.py
@@ -1,36 +1,47 @@
 from random import randint
 
 #create a list of play options
-t = ["Rock", "Paper", "Scissors"]
+t = ["rock", "paper", "scissors"]
+
+def print_score(comp_score, player_score):
+    print("Computer's Score:", comp_score, "Player's score:", player_score, "\n")
 
 #assign a random play to the computer
 computer = t[randint(0,2)]
 
-#set player to False
-player = False
+#Initialise scores
+player_score = 0
+comp_score = 0
 
-while player == False:
-#set player to True
-    player = input("Rock, Paper, Scissors?")
-    if player == computer:
-        print("Tie!")
-    elif player == "Rock":
-        if computer == "Paper":
-            print("You lose!", computer, "covers", player)
+while True:
+    player_input = input("Rock, Paper, Scissor? Else type Quit?\n")
+    if player_input.lower() == "quit":
+        print_score(comp_score, player_score)
+        break
+    elif player_input.lower() == computer.lower():
+        print("Tie! No points for both!")
+    elif player_input.lower() == "rock":
+        if computer == "paper":
+            print("You lose!", computer, "covers", player_input)
+            comp_score+=1
         else:
-            print("You win!", player, "smashes", computer)
-    elif player == "Paper":
-        if computer == "Scissors":
-            print("You lose!", computer, "cut", player)
+            print("You win!", player_input, "smashes", computer)
+            player_score+=1
+    elif player_input.lower() == "paper":
+        if computer == "scissors":
+            print("You lose!", computer, "cut", player_input)
+            comp_score+=1
         else:
-            print("You win!", player, "covers", computer)
-    elif player == "Scissors":
-        if computer == "Rock":
-            print("You lose...", computer, "smashes", player)
+            print("You win!", player_input, "covers", computer)
+            player_score+=1
+    elif player_input.lower() == "scissors":
+        if computer == "rock":
+            print("You lose...", computer, "smashes", player_input)
+            comp_score+=1
         else:
-            print("You win!", player, "cut", computer)
+            print("You win!", player_input, "cut", computer)
+            player_score+=1
     else:
-        print("That's not a valid play. Check your spelling!")
-    #player was set to True, but we want it to be False so the loop continues
-    player = False
+        print("That's not a valid entry. Check your spelling!")
     computer = t[randint(0,2)]
+
